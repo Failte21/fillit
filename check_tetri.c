d10abe1d95fae62938c51e52bf0c2848b5e93b49
Benoit CORBEL <becorbel@e3r5p7.42.fr>
2016-11-15 16:05:46 +0100

    ajout

--- check_tetri.c
+++ check_tetri.c
@@ -6,17 +6,20 @@
 /*   By: becorbel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 16:51:54 by becorbel          #+#    #+#             */
-/*   Updated: 2016/11/14 19:43:30 by becorbel         ###   ########.fr       */
+/*   Updated: 2016/11/15 16:05:15 by becorbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+#include <stdio.h>
 
 int	check_tetri(char *s, int i)
 {
 	int	j;
 	int	n;
 
+	n = 0;
 	while (s[i])
 	{
 		j = 0;
@@ -30,10 +33,17 @@ int	check_tetri(char *s, int i)
 		n++;
 		if (j != 4)
 			return (0);
-		else
-			i++;
+		if (n == 4)
+		{
+			if (s[i + 1] != '\n' && s[i + 1] != '\0')
+				return (0);
+			if (s[i + 1] == '\n')
+				if((check_tetri(s, i + 2)) == 1)
+					return (1);
+			if (s[i + 1] == '\0')
+				return (1);
+		}
+		i++;
 	}
-	if (n != 4)
-		return (0);
 	return (1);
 }
