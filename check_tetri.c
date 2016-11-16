d8f44deca9aab39fa69f6ffdb477bc2417b8818d
Benoit CORBEL <becorbel@e3r2p15.42.fr>
2016-11-16 19:12:28 +0100

    CHECK PRESQUE FINI

--- check_tetri.c
+++ check_tetri.c
@@ -6,7 +6,7 @@
 /*   By: becorbel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/14 16:51:54 by becorbel          #+#    #+#             */
-/*   Updated: 2016/11/16 12:39:09 by lsimon           ###   ########.fr       */
+/*   Updated: 2016/11/16 19:11:08 by becorbel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,50 +60,45 @@ int	check_tetri(char *s, int i)
 {
 	while (s[i])
 	{
-		while (s[i] != '\n' && s[i + 1] != '\n' && s[i])
-		{
-			if ((s[i] != '#') || ((s[i] == '#') && ((s[i + 1] == '#')
-							|| (i > 0 && (s[i - 1] == '#'))
-							|| (s[i + 5] == '#')
-							|| (i > 6 && (s[i - 5] == '#')))))
-				i++;
-			else
-				return (0);
-		}
-		if (check_tetri(s, i + 2) == 0)
+		if ((s[i] != '#') || ((s[i] == '#') && ((s[i + 1] == '#')
+						|| (i > 0 && (s[i - 1] == '#'))
+						|| (s[i + 5] == '#')
+						|| (i > 6 && (s[i - 5] == '#')))))
+			i++;
+		else
 			return (0);
-		i++;
+		if (s[i] == '\n' && s[i + 1] == '\n')
+		{
+			if (check_tetri(s, i + 2) == 0)
+				return (0);
+			return (1);
+		}
 	}
 	return (1);
 }
 
-int	check_nb_piece(char *s)
+int	check_nb_piece(char *s, int i)
 {
 	int	j;
-	int i;
 
-	i = 0;
 	j = 0;
 	while (s[i])
 	{
-		while (i < 19)
+		if (s[i] == '#')
+			j++;
+		if (s[i] == '\n' && s[i + 1] == '\n')
 		{
-			if (s[i] == '#')
-				j++;
-			i++;
-		}
-		//ft_putnbr(j);
-		//ft_putchar('\n');
-		if (j == 4)
-		{
-			//ft_putendl("OK");
-			if (s[i + 1] == '\n')
-				if (check_nb_piece(&s[i + 2]) == 1)
-					return (1);
-			return (1);
+			if (j == 4)
+			{
+				if (check_nb_piece(s, i + 2) == 0)
+					return (0);
+				return (1);
+			}
 		}
 		i++;
 	}
+	if (j == 4)
+		return (1);
 	return (0);
 }
 
@@ -113,19 +108,10 @@ int	all_check(char *s)
 
 	i = 0;
 	if (!(check_str(s, i)))
-	{
-		ft_putendl("checkstr");
 		return (0);
-	}
-	if (!(check_nb_piece(s)))
-	{
-		ft_putendl("checknb");
+	if (!(check_nb_piece(s, i)))
 		return (0);
-	}
 	if (!(check_tetri(s, i)))
-	{
-		ft_putendl("checktetri");
 		return (0);
-	}
 	return (1);
 }
